be00cafd8e3f5e61ef02e19f859951b980d40db2
JIEUN233 <lje030522@gmail.com>
2026-09-21 21:27:06 +0900

    feat: 미니 실습 - MovieCard 컴포넌트 분리 및 재사용

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import MovieCard from "./components/movie-card";
+import "./App.css";
 
 function App() {
   return (
-    <h1>진진의 React 학습</h1>
-  )
+    <main>
+      <MovieCard title="오디세이" releaseDate="2026.08.05" isBookmarked={true} />
+      <MovieCard title="토이 스토리 5" releaseDate="2026.06.17" isBookmarked={false} />
+      <MovieCard title="듄: 파트 3" releaseDate="2027.01.15" isBookmarked={true} />
+    </main>
+  );
 }
 
-export default App
+export default App;

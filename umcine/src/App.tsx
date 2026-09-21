c057ae9cb1bcde886ae25e79d094e167954902d3
JIEUN233 <lje030522@gmail.com>
2026-09-21 21:29:14 +0900

    feat: 미니 실습 - map으로 영화 목록 렌더링

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,14 +1,32 @@
-import MovieCard from "./components/movie-card";
 import "./App.css";
 
-function App() {
+interface Movie {
+  id: number;
+  title: string;
+  releaseDate: string;
+}
+
+const movies: Movie[] = [
+  { id: 1, title: "오디세이", releaseDate: "2026.08.05" },
+  { id: 2, title: "토이 스토리 5", releaseDate: "2026.06.17" },
+  { id: 3, title: "듄: 파트 3", releaseDate: "2027.01.15" },
+];
+
+export default function App() {
   return (
     <main>
-      <MovieCard title="오디세이" releaseDate="2026.08.05" isBookmarked={true} />
-      <MovieCard title="토이 스토리 5" releaseDate="2026.06.17" isBookmarked={false} />
-      <MovieCard title="듄: 파트 3" releaseDate="2027.01.15" isBookmarked={true} />
+      <h1>영화 목록</h1>
+      {movies.length === 0 ? (
+        <p>표시할 영화가 없어요.</p>
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              {movie.title} - {movie.releaseDate}
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
-
-export default App;
